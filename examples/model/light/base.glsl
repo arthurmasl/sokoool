9a6ea443db9377b15eef2519e82bbcba56e97d1f
Artur Maslobojev <arthurmasl@gmail.com>
2025-04-13 17:53:22 +0300

    add example

---
+++ examples/model/light/base.glsl
@@ -0,0 +1,167 @@
+#pragma sokol @header package game
+#pragma sokol @header import sg "sokol/gfx"
+#pragma sokol @ctype mat3 Mat3
+#pragma sokol @ctype mat4 Mat4
+#pragma sokol @ctype vec3 Vec3
+#pragma sokol @ctype vec4 Vec4
+
+// VERTEX SHADER
+#pragma sokol @vs vs
+in vec3 aPosition;
+in vec3 aNormal;
+in vec2 aTexCoord;
+
+out vec3 vPosition;
+out vec3 vNormal;
+out vec2 vTexCoord;
+
+layout(binding = 0) uniform vs_params {
+    mat4 uModel;
+    mat4 uView;
+    mat4 uProjection;
+};
+
+void main() {
+    gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);
+
+    vPosition = vec3(uModel * vec4(aPosition, 1.0));
+    vNormal = mat3(uModel) * aNormal;
+    vTexCoord = aTexCoord;
+}
+#pragma sokol @end
+
+// FRAGMENT SHADER
+#pragma sokol @fs fs
+in vec3 vPosition;
+in vec3 vNormal;
+in vec2 vTexCoord;
+
+out vec4 frag_color;
+
+layout(binding = 1) uniform fs_params {
+    vec3 uViewPos;
+    float uMaterialShininess;
+};
+
+// layout(binding = 0) uniform texture2D uDiffuseTexture;
+// layout(binding = 0) uniform sampler uDiffuseTextureSmp;
+// #define sampled_texture sampler2D(uDiffuseTexture, uDiffuseTextureSmp)
+// layout(binding = 1) uniform texture2D uSpecularTexture;
+// layout(binding = 1) uniform sampler uSpecularTextureSmp;
+// #define sampled_texture sampler2D(uSpecularTexture, uSpecularTextureSmp)
+layout(binding = 0) uniform texture2D uTexture;
+layout(binding = 0) uniform sampler uTextureSmp;
+#define sampled_texture sampler2D(uTexture, uTextureSmp)
+
+layout(binding = 2) uniform fs_dir_light {
+    vec3 uDirection;
+    vec3 uAmbient;
+    vec3 uDiffuse;
+    vec3 uSpecular;
+} dir_light;
+
+#define NR_POINT_LIGHTS 1
+
+layout(binding = 3) uniform fs_point_lights {
+    vec4 uPosition[NR_POINT_LIGHTS];
+    vec4 uAmbient[NR_POINT_LIGHTS];
+    vec4 uDiffuse[NR_POINT_LIGHTS];
+    vec4 uSpecular[NR_POINT_LIGHTS];
+    vec4 uAttenuation[NR_POINT_LIGHTS];
+} point_lights;
+
+struct dir_light_t {
+    vec3 uDirection;
+    vec3 uAmbient;
+    vec3 uDiffuse;
+    vec3 uSpecular;
+};
+
+struct point_light_t {
+    vec3 uPosition;
+    float constant;
+    float linear;
+    float quadratic;
+    vec3 uAmbient;
+    vec3 uDiffuse;
+    vec3 uSpecular;
+};
+
+dir_light_t get_directional_light();
+point_light_t get_point_light(int index);
+
+vec3 calc_dir_light(dir_light_t light, vec3 normal, vec3 view_dir);
+vec3 calc_point_light(point_light_t light, vec3 normal, vec3 frag_pos, vec3 view_dir);
+
+void main() {
+    vec3 norm = normalize(vNormal);
+    vec3 view_dir = normalize(uViewPos - vPosition);
+
+    vec3 result = calc_dir_light(get_directional_light(), norm, view_dir);
+    for (int i = 0; i < NR_POINT_LIGHTS; ++i) {
+        result += calc_point_light(get_point_light(i), norm, vPosition, view_dir);
+    }
+
+    frag_color = vec4(result, 1.0);
+}
+
+dir_light_t get_directional_light() {
+    return dir_light_t(
+        dir_light.uDirection,
+        dir_light.uAmbient,
+        dir_light.uDiffuse,
+        dir_light.uSpecular
+    );
+}
+
+point_light_t get_point_light(int index) {
+    int i = index;
+    return point_light_t(
+        point_lights.uPosition[i].xyz,
+        point_lights.uAttenuation[i].x,
+        point_lights.uAttenuation[i].y,
+        point_lights.uAttenuation[i].z,
+        point_lights.uAmbient[i].xyz,
+        point_lights.uDiffuse[i].xyz,
+        point_lights.uSpecular[i].xyz
+    );
+}
+
+vec3 calc_dir_light(dir_light_t light, vec3 normal, vec3 view_dir) {
+    vec3 light_dir = normalize(-light.uDirection);
+    // diffuse shading
+    float diff = max(dot(normal, light_dir), 0.0);
+    // specular shading
+    vec3 reflect_dir = reflect(-light_dir, normal);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), uMaterialShininess);
+    // combine results
+    vec3 ambient = light.uAmbient * vec3(texture(sampled_texture, vTexCoord));
+    vec3 diffuse = light.uDiffuse * diff * vec3(texture(sampled_texture, vTexCoord));
+    vec3 specular = light.uSpecular * spec * vec3(texture(sampled_texture, vTexCoord));
+    return (ambient + diffuse + specular);
+}
+
+vec3 calc_point_light(point_light_t light, vec3 normal, vec3 frag_pos, vec3 view_dir) {
+    vec3 light_dir = normalize(light.uPosition - frag_pos);
+    // diffuse shading
+    float diff = max(dot(normal, light_dir), 0.0);
+    // specular shading
+    vec3 reflect_dir = reflect(-light_dir, normal);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), uMaterialShininess);
+    // attenuation
+    float distance = length(light.uPosition - frag_pos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance +
+                light.quadratic * (distance * distance));
+    // combine results
+    vec3 ambient = light.uAmbient * vec3(texture(sampled_texture, vTexCoord));
+    vec3 diffuse = light.uDiffuse * diff * vec3(texture(sampled_texture, vTexCoord));
+    vec3 specular = light.uSpecular * spec * vec3(texture(sampled_texture, vTexCoord));
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+    return (ambient + diffuse + specular);
+}
+
+#pragma sokol @end
+
+#pragma sokol @program base vs fs
---
+++ examples/model/light/utils.odin
@@ -0,0 +1,167 @@
+package game
+
+import "base:intrinsics"
+import "core:fmt"
+import "core:image/png"
+import "core:log"
+import "core:os"
+import "core:strings"
+import "vendor:cgltf"
+
+import "web"
+
+import sg "sokol/gfx"
+
+_ :: os
+_ :: web
+
+IS_WEB :: ODIN_ARCH == .wasm32 || ODIN_ARCH == .wasm64p32
+
+// Wraps os.read_entire_file and os.write_entire_file, but they also work with emscripten.
+@(require_results)
+read_entire_file :: proc(
+  name: string,
+  allocator := context.allocator,
+  loc := #caller_location,
+) -> (
+  data: []byte,
+  success: bool,
+) {
+  when IS_WEB {
+    return web.read_entire_file(name, allocator, loc)
+  } else {
+    return os.read_entire_file(name, allocator, loc)
+  }
+}
+
+write_entire_file :: proc(name: string, data: []byte, truncate := true) -> (success: bool) {
+  when IS_WEB {
+    return web.write_entire_file(name, data, truncate)
+  } else {
+    return os.write_entire_file(name, data, truncate)
+  }
+}
+
+load_image :: proc(name: string) -> ^png.Image {
+  img_data, img_data_ok := read_entire_file(name, context.temp_allocator)
+  if !img_data_ok {
+    log.error("Failed loading texture")
+  }
+
+  img, img_err := png.load_from_bytes(img_data, nil, context.temp_allocator)
+  if img_err != nil {
+    log.error(img_err)
+  }
+
+  return img
+}
+
+load_object :: proc(name: string) {
+  options: cgltf.options
+  path := strings.unsafe_string_to_cstring(name)
+
+  data, result := cgltf.parse_file(options, path)
+  if result != .success {
+    fmt.println("Failed to parse file", result)
+  }
+  defer cgltf.free(data)
+
+  result = cgltf.load_buffers(options, data, path)
+  if result != .success {
+    fmt.println("Failed to load buffers")
+    return
+  }
+
+  // tm: [16]f32
+  // cgltf.node_transform_world(data.animations[0].channels[1].target_node, &tm[0])
+
+  for mesh in data.meshes {
+    for p in mesh.primitives {
+      position_arr: []f32
+      normal_arr: []f32
+      texcoord_arr: []f32
+
+      for a in p.attributes {
+        num_floats := cgltf.accessor_unpack_floats(a.data, nil, 0)
+        if a.type == .position {
+          position_arr = make([]f32, num_floats, context.temp_allocator)
+          _ = cgltf.accessor_unpack_floats(a.data, &position_arr[0], num_floats)
+        }
+        if a.type == .normal {
+          normal_arr = make([]f32, num_floats, context.temp_allocator)
+          _ = cgltf.accessor_unpack_floats(a.data, &normal_arr[0], num_floats)
+        }
+        if a.type == .texcoord {
+          texcoord_arr = make([]f32, num_floats, context.temp_allocator)
+          _ = cgltf.accessor_unpack_floats(a.data, &texcoord_arr[0], num_floats)
+        }
+      }
+
+      vertices := make([dynamic]f32, context.temp_allocator)
+      vertices_count := (len(position_arr) + len(normal_arr) + len(texcoord_arr)) / 8
+
+      for i in 0 ..< vertices_count {
+        append(&vertices, ..position_arr[i * 3:(i * 3) + 3])
+        append(&vertices, ..normal_arr[i * 3:(i * 3) + 3])
+        append(&vertices, ..texcoord_arr[i * 2:(i * 2) + 2])
+      }
+
+      indices_count := cgltf.accessor_unpack_indices(p.indices, nil, 0, 0)
+      indices := make([]u16, indices_count, context.temp_allocator)
+      _ = cgltf.accessor_unpack_indices(p.indices, &indices[0], size_of(u16), indices_count)
+
+      image_buffer := data.textures[0].image_.buffer_view
+      texture_bytes := cgltf.buffer_view_data(image_buffer)
+      texture := ([]byte)(texture_bytes[:image_buffer.size])
+
+      img, img_err := png.load_from_bytes(texture, nil, context.temp_allocator)
+      if img_err != nil {
+        fmt.println(img_err)
+        return
+      }
+
+      g.mesh.bindings.vertex_buffers[0] = sg.make_buffer(
+        {data = {ptr = &vertices[0], size = uint(size_of(f32) * 8 * vertices_count)}},
+      )
+      g.mesh.bindings.index_buffer = sg.make_buffer(
+        {
+          type = .INDEXBUFFER,
+          data = {ptr = &indices[0], size = uint(size_of(u16) * indices_count)},
+        },
+      )
+      g.mesh.bindings.images[IMG_uTexture] = sg.make_image(
+        {
+          width = i32(img.width),
+          height = i32(img.height),
+          data = {
+            subimage = {
+              0 = {0 = {ptr = raw_data(img.pixels.buf), size = uint(img.width * img.height * 4)}},
+            },
+          },
+        },
+      )
+      g.mesh.bindings.samplers[SMP_uTextureSmp] = sg.make_sampler({})
+
+      g.mesh.face_count = len(indices)
+    }
+  }
+
+  free_all(context.temp_allocator)
+}
+
+sg_range :: proc {
+  sg_range_from_slice,
+  sg_range_from_struct,
+}
+
+sg_range_from_slice :: proc(sl: []$T) -> sg.Range {
+  return {ptr = raw_data(sl), size = uint(slice.size(sl))}
+}
+
+sg_range_from_struct :: proc(st: ^$T) -> sg.Range where intrinsics.type_is_struct(T) {
+  when T == png.Image {
+    return {ptr = raw_data(st.pixels.buf), size = uint(st.width * st.height * 4)}
+  }
+
+  return {ptr = st, size = size_of(T)}
+}
