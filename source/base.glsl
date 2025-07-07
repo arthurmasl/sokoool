90dfb653caf3c0734a5f2da173bc59e0bb3504fa
Artur Maslobojev <arthurmasl@gmail.com>
2025-07-07 23:22:42 +0300

    shader

--- source/base.glsl
+++ source/base.glsl
@@ -1,28 +1,24 @@
 #pragma sokol @header package game
-
 #pragma sokol @header import sg "sokol/gfx"
-#pragma sokol @ctype vec3 Vec3
-#pragma sokol @ctype mat4 Mat4
 
+// VS
 #pragma sokol @vs vs
-
-in vec2 pos;
-in vec2 uv;
-
 layout(binding = 0) uniform vs_params {
     float time;
 };
+in vec2 pos;
 
 void main() {
     gl_Position = vec4(pos, 0.0, 1.0);
 }
 #pragma sokol @end
 
+// FS
 #pragma sokol @fs fs
 out vec4 frag_color;
 
 void main() {
-    frag_color = vec4(0.8, 0.8, 0.8, 1.0);
+    frag_color = vec4(0.2, 0.8, 0.8, 1.0);
 }
 
 #pragma sokol @end
